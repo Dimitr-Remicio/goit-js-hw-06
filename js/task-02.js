7b37d3b6cbcb7e473289da6583e729b97f8fc981
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-03-10 20:11:57 -0500

    cloning repository

---
+++ js/task-02.js
@@ -0,0 +1,48 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+
+const list = document.querySelector('#ingredients');
+
+
+const firstItem = document.createElement('li');
+firstItem.textContent = 'Potatoes';
+list.prepend(firstItem);
+document.querySelector('li').classList.add('item');
+
+
+const secondItem = document.createElement('li');
+secondItem.textContent = 'Mushrooms';
+list.prepend(secondItem);
+document.querySelector('li').classList.add('item');
+
+
+const thirdItem = document.createElement('li');
+thirdItem.textContent = 'Garlic';
+list.prepend(thirdItem);
+document.querySelector('li').classList.add('item');
+
+
+const fourthItem = document.createElement('li');
+fourthItem.textContent = 'Tomatos';
+list.prepend(fourthItem);
+document.querySelector('li').classList.add('item');
+
+
+const fiveItem = document.createElement('li');
+fiveItem.textContent = 'Herbs';
+list.prepend(fiveItem);
+document.querySelector('li').classList.add('item');
+
+
+const lastItem = document.createElement('li');
+lastItem.textContent = 'Condiments';
+list.prepend(lastItem);
+document.querySelector('li').classList.add('item');
+
---
+++ js/task-03.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const list = document.querySelector('.gallery');
+
+const image1 = document.createElement("img");
+image1.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+image1.alt = 'White and Black Long Fur Cat';
+list.prepend(image1);
+
+const image2 = document.createElement("img");
+image2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+image2.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
+list.prepend(image2);
+
+const image3 = document.createElement("img");
+image3.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
+image3.alt = 'Group of Horses Running';
+list.prepend(image3/);
+
