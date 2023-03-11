f7b9eb8fa9bc5d575ba3e9f4a77e465433bad680
Dimitr-Remicio <113401276+Dimitr-Remicio@users.noreply.github.com>
2023-03-10 21:12:46 -0500

    update

--- js/task-03.js
+++ js/task-03.js
@@ -13,20 +13,13 @@ const images = [
   },
 ];
 
-const list = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 
-const image1 = document.createElement("img");
-image1.src = 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-image1.alt = 'White and Black Long Fur Cat';
-list.prepend(image1);
+const galleryArray = images.map(({url,alt}) => {
+  const galleryItem = document.createElement('li');
+  galleryItem.classList.add('gallery__item');
+  galleryItem.insertAdjacentHTML('beforeend', `<img src="${url}" class="gallery__image" alt="${alt}">`);
+  return galleryItem;
+});
 
-const image2 = document.createElement("img");
-image2.src = 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-image2.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
-list.prepend(image2);
-
-const image3 = document.createElement("img");
-image3.src = 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-image3.alt = 'Group of Horses Running';
-list.prepend(image3/);
-
+gallery.append(...galleryArray);
